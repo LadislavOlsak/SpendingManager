225c68067a8041b3ee6bf84cf1bfa0727ae2af30
422255 <422255@mail.muni.cz>
2018-03-26 22:13:30 +0200

    Initial commit

---
+++ app/src/main/java/android/spendingmanager/pv239/muni/fi/cz/spendingmanager/MainActivity.kt
@@ -0,0 +1,12 @@
+package android.spendingmanager.pv239.muni.fi.cz.spendingmanager
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
