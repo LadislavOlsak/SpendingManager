5fca0b49f2665b1df8b9ac5e9e917f194af1e8dc
422255 <422255@mail.muni.cz>
2018-04-17 23:45:05 +0200

    Loyalty cards +Delete/Edit/New

--- app/src/main/java/android/spendingmanager/pv239/muni/fi/cz/spendingmanager/loyaltycards/LoyaltyCardsActivity.kt
+++ app/src/main/java/android/spendingmanager/pv239/muni/fi/cz/spendingmanager/loyaltycards/LoyaltyCardsActivity.kt
@@ -6,8 +6,11 @@ import android.graphics.Color
 import android.os.Bundle
 import android.spendingmanager.pv239.muni.fi.cz.spendingmanager.R
 import android.spendingmanager.pv239.muni.fi.cz.spendingmanager.general.ScanActivity
+import android.spendingmanager.pv239.muni.fi.cz.spendingmanager.general.ViewMode
 import android.support.v7.app.AppCompatActivity
+import com.google.gson.Gson
 import kotlinx.android.synthetic.main.activity_loyalty_cards.*
+import kotlinx.android.synthetic.main.loyalty_card_item.*
 
 class LoyaltyCardsActivity : AppCompatActivity() {
 
@@ -17,16 +20,24 @@ class LoyaltyCardsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_loyalty_cards)
-        fab.setOnClickListener { startScanActivity() }
 
+        setListeners()
+    }
+
+    private fun setListeners() {
         val loyaltyCards = listOf(
-            LoyaltyCard("501928794164", "MultiSport", Color.BLUE),
-            LoyaltyCard("971R8B261", "Billa", Color.MAGENTA),
-            LoyaltyCard("1234567890", "Tesco", Color.YELLOW),
-            LoyaltyCard("S421234567890", "ISIC", Color.GREEN)
+                LoyaltyCard("501928794164", "MultiSport", Color.BLUE),
+                LoyaltyCard("971R8B261", "Billa", Color.MAGENTA),
+                LoyaltyCard("1234567890", "Tesco", Color.YELLOW),
+                LoyaltyCard("S421234567890", "ISIC", Color.GREEN)
         )
-
         loyalty_cards_list.adapter = LoyaltyCardsAdapter(this, loyaltyCards)
+
+        fab.setOnClickListener { startScanActivity() }
+        loyalty_cards_list.setOnItemClickListener { adapterView, view, i, l ->
+            val selectedCard = loyalty_cards_list.adapter.getItem(i) as LoyaltyCard
+            startLoyaltyCardActivity(selectedCard)
+        }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -46,13 +57,20 @@ class LoyaltyCardsActivity : AppCompatActivity() {
     private fun startNewLoyaltyCardActivity(cardNumber : String?) {
         val newCardActivity = Intent(this, NewLoyaltyCardActivity::class.java)
         newCardActivity.putExtra("cardNumber", cardNumber)
+        newCardActivity.putExtra("mode", ViewMode.New.toString())
         startActivity(newCardActivity)
         //todo refresh view
     }
 
     private fun startScanActivity() {
-        val scanIntent = Intent(this@LoyaltyCardsActivity, ScanActivity::class.java)
+        val scanIntent = Intent(this, ScanActivity::class.java)
         scanIntent.putExtra("parameterName", parameterName)
         startActivityForResult(scanIntent, SCAN_ACTIVITY_REQUEST_CODE)
+    }
+
+    private fun startLoyaltyCardActivity(card : LoyaltyCard) {
+        val intent = Intent(this, LoyaltyCardActivity::class.java)
+        intent.putExtra("card", Gson().toJson(card))
+        startActivity(intent)
     }
 }
